7d0934a8ec061d3d7d6cc8dc0b29d01a7db3ea03
carlos sigua <carlos.sigua@gmail.com>
2023-05-22 21:10:03 -0500

    listo de nuevo veamos que sucede con las llaves de esa manera

--- install-utils/install.sh
+++ install-utils/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Verificar la distribucion actual
-if `[[ -f /etc/lsb-release ]]`; then
+if [ -f /etc/lsb-release ]; then
     # Distribucion basada en Ubuntu
     DISTRO="ubuntu"
-elif `[[ -f /etc/fedora-release ]]`; then
+elif [ -f /etc/fedora-release ]; then
     # Distribucion basada en Fedora
     DISTRO="fedora"
 else
@@ -13,11 +13,11 @@ else
 fi
 
 # Instalar las librerias según la distribucion
-if `[[ $DISTRO == "ubuntu" ]]`; then
+if [ $DISTRO == "ubuntu" ]; then
     # Instalar las librerias en Ubuntu
     sudo apt-get update
     sudo apt-get install -y curl zsh ncdu git htop
-elif `[[ $DISTRO == "fedora" ]]`; then
+elif [ $DISTRO == "fedora" ]; then
     # Instalar las librerias en Fedora
     sudo dnf update
     sudo dnf install -y curl zsh ncdu git htop
