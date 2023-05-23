fb4d2cef6da6f277733955d917bac71151bb1b6f
carlos sigua <carlos.sigua@gmail.com>
2023-05-22 20:51:50 -0500

    agregando paquete htop

--- install-utils/install.sh
+++ install-utils/install.sh
@@ -16,9 +16,9 @@ fi
 if `[[ $DISTRO == "ubuntu" ]]`; then
     # Instalar las librerias en Ubuntu
     sudo apt-get update
-    sudo apt-get install -y curl zsh ncdu git 
+    sudo apt-get install -y curl zsh ncdu git htop
 elif `[[ $DISTRO == "fedora" ]]`; then
     # Instalar las librerias en Fedora
     sudo dnf update
-    sudo dnf install -y curl zsh ncdu git
+    sudo dnf install -y curl zsh ncdu git htop
 fi
