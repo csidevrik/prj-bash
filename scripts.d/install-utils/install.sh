df647d3b7bb0eea9f9a3c24dad507a0d992fb342
carlos sigua <carlos.sigua@gmail.com>
2023-08-06 14:13:39 -0500

    comiteado

--- scripts.d/install-utils/install.sh
+++ scripts.d/install-utils/install.sh
@@ -4,16 +4,48 @@
 packages_ubuntu=("neovim" "curl" "git" "ncdu" "zsh" "htop" "screenfetch" "openssh-server" "openssl" "sqlite")
 packages_fedora=("neovim" "curl" "git" "ncdu" "zsh" "htop" "screenfetch" "openssh-server" "openssl" "sqlite")
 
+# Colores de formato
+FMT_RESET=$(tput sgr0)
+FMT_BOLD=$(tput bold)
+FMT_RED=$(tput setaf 1)
+FMT_GREEN=$(tput setaf 2)
+FMT_YELLOW=$(tput setaf 3)
+FMT_BLUE=$(tput setaf 34)
+FMT_PURPLE=$(tput setaf 35)
+
+
+# Función para imprimir mensajes de éxito
+print_success() {
+  printf '\n%s%s%s\n' "${FMT_GREEN}${FMT_BOLD}" "¡Instalación exitosa!" "${FMT_RESET}"
+  printf '%sAhora tienes instalados los siguientes paquetes:\n' "${FMT_YELLOW}"
+  printf '%s\n' "${packages_ubuntu[@]}"  # Si es Ubuntu, muestra los paquetes
+  printf '%s\n' "${packages_fedora[@]}"  # Si es Fedora, muestra los paquetes
+  printf '%s\n' "${FMT_RESET}"
+  printf '%sEjecuta "zsh" para probar tu nuevo shell.\n' "${FMT_YELLOW}"
+  printf '%sSi prefieres mantener tu shell actual, reinicia tu sesión para aplicar los cambios.\n' "${FMT_YELLOW}"
+  printf '%s¡Disfruta de tu nueva configuración de shell!\n' "${FMT_RESET}"
+}
+
+# Función para instalar repositorios de RPM Fusion en Fedora
+install_rpmfusion_repos() {
+    local version=$1
+    sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$version.noarch.rpm"
+    sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$version.noarch.rpm"
+}
+
+
 main(){
     # Verificar la distribucion actual
     if [ -f /etc/lsb-release ]; then
     # Distribucion basada en Ubuntu
         DISTRO="ubuntu"
+        printf "${FMT_RESET}${FMT_PURPLE}${FMT_BOLD}Estas en una distro de Ubuntu${FMT_RESET}\n"
     elif [ -f /etc/fedora-release ]; then
     # Distribucion basada en Fedora
         DISTRO="fedora"
+        printf "${FMT_RESET}${FMT_BLUE}${FMT_BOLD}Estas en una distro de Ubuntu${FMT_RESET}\n"
     else
-        echo "No se pudo determinar la distribución actual"
+        printf "${FMT_RESET}${FMT_RED}${FMT_BOLD}No se pudo determinar la distribución actual${FMT_RESET}\n"
         exit 1
     fi
     # Instalar las librerias según la distribucion
@@ -37,16 +69,13 @@ main(){
 
         fi
     fi
+
+    # Llamada a la función de impresión de éxito
+    print_success
 }    
 
 
 
-# Función para instalar repositorios de RPM Fusion en Fedora
-install_rpmfusion_repos() {
-    local version=$1
-    sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$version.noarch.rpm"
-    sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$version.noarch.rpm"
-}
 
 #LLamada a la funcion principal (main)
 main
