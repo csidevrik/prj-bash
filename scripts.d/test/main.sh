fe55229fdaf2685742b3d67ceb74bc47359f074d
carlos sigua <carlos.sigua@gmail.com>
2024-05-12 20:18:05 -0500

    scripting bash

--- scripts.d/test/main.sh
+++ scripts.d/test/main.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-source lib/colores.sh
 
-print_purple "solo es purpura"
-print_white "subjetivo"
+# Descargar el script remoto
+wget https://raw.githubusercontent.com/csidevrik/prj-bash/main/scripts.d/test/lib/colores.sh -O /tmp/colores.sh
+
+# Verificar si la descarga fue exitosa
+if [ $? -eq 0 ]; then
+    # Incluir el script descargado
+    source /tmp/colores.sh
+    # Ahora puedes usar las funciones del script 'colores.sh'
+    print_purple "solo es purpura"
+    print_white "subjetivo"
+
+    # Limpieza: eliminar el script descargado después de usarlo
+    rm /tmp/colores.sh
+else
+    # Si la descarga falla, mostrar un mensaje de error
+    echo "Error: No se pudo descargar el script 'colores.sh' desde GitHub."
+fi
+
+
 
 
