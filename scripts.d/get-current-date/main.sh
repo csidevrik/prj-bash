8bc74705346bbfd989c922e0c125db3ccc18d125
cs m720q <carlos.sigua@gmail.com>
2024-08-23 10:12:06 -0500

    solo vale el main.sh

--- scripts.d/get-current-date/main.sh
+++ scripts.d/get-current-date/main.sh
@@ -2,7 +2,7 @@
 
 function Get-Current-Date() {
     # Obtiene la fecha y hora actual
-    current_date=$(date '+%Y %m %d %H %M %S')
+    current_date=$(date '+%Y %-m %-d %H %M %S')
     year=$(echo $current_date | awk '{print $1}')
     month=$(echo $current_date | awk '{print $2}')
     day=$(echo $current_date | awk '{print $3}')
@@ -16,7 +16,7 @@ function Get-Current-Date() {
     case $1 in
         -JustDate)
             # Formato para solo la fecha
-            result=$(printf "%04d%02d%s-%02d_" $year $month ${months[$((month-1))]} $day)
+            result=$(printf "%04d%s-%02d_" $year ${months[$((month-1))]} $day)
             ;;
         -JustHour)
             # Formato para solo la hora
@@ -37,13 +37,14 @@ function Get-Current-Date() {
 
 # Ejemplos de uso:
 # Para obtener solo la fecha:
-Get-Current-Date -JustDate
+Get-Current-Date $1
 
 # Para obtener solo la hora:
-Get-Current-Date -JustHour
+# Get-Current-Date -JustHour
 
 # Para obtener la fecha y hora (con mes y día):
-Get-Current-Date -DayAndHour
+# Get-Current-Date -DayAndHour
 
 # Para obtener la fecha completa (default):
-Get-Current-Date
+# Get-Current-Date
+
