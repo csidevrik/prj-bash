fb95f3656f0c383bd808642f7bb8ee1fa9db88c1
cs w320p <carlos.sigua@gmail.com>
2024-08-22 20:50:28 -0500

    se sube para evaluacion

---
+++ scripts.d/get-current-date/main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Mapa de meses en español
+declare -A months=(
+    [1]="ENE" [2]="FEB" [3]="MAR" [4]="ABR"
+    [5]="MAY" [6]="JUN" [7]="JUL" [8]="AGO"
+    [9]="SEP" [10]="OCT" [11]="NOV" [12]="DIC"
+)
+
+# Obtener la fecha y hora actual
+actualDate=$(date +'%Y-%m-%d %H:%M:%S')
+day=$(date +'%d')
+month=$(date +'%m')
+year=$(date +'%Y')
+hour=$(date +'%H')
+minute=$(date +'%M')
+second=$(date +'%S')
+
+# Formato para diferentes opciones
+case "$1" in
+    --justDate)
+        resultado="${day}${months[$month]}_${day}${year}"
+        ;;
+    --justHour)
+        resultado="${hour}h${minute}m${second}s"
+        ;;
+    --dayAndHour)
+        resultado="${months[$month]}${day}_${hour}h${minute}m${second}s"
+        ;;
+    *)
+        resultado="${day}${months[$month]}_${day}${year}_${hour}${minute}${second}"
+        ;;
+esac
+
+# Mostrar el resultado
+echo "$resultado"
