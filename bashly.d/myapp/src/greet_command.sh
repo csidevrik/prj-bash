46ec1fcb56349d29af96c441e0e0de7167d2d142
linkfedok <carlos.sigua@gmail.com>
2024-09-01 13:50:26 -0500

    prueba de app con bashly

---
+++ bashly.d/myapp/src/greet_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/greet_command.sh'"
+echo "# code for 'myapp greet' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
---
+++ bashly.d/myapp/src/help_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/help_command.sh'"
+echo "# Esta es toda la ayuda que encontraras"
+echo "# Si te gusta bien sino ya nada"
+inspect_args
