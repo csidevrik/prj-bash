50328dc27f7418a146e7f51c74c588aea8b3ee32
iroh <carlos.sigua@gmail.com>
2026-03-20 22:27:12 -0500

    ya tenemos google drive y onedrive para linux aunque podria mejorarse

--- scripts.d/install-googledrivep/rclone-googledrive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LOGFILE=/var/log/rclonegoogledrivep.log
-/usr/bin/rclone --vfs-cache-mode writes mount "GoogleDriveP": ~/GoogleDriveP &> $LOGFILE &
-if [ $? -eq 0 ]; then
-	/usr/bin/notify-send "Google Drive" "Google Drive successfully mounted."
-	echo "Mounted succesfully" >> "$LOGFILE"
-else
-	echo "Failed to mount GoogleDrive" >> "$LOGFILE"
-fi
